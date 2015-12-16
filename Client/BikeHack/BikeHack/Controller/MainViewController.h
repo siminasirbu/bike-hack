065496f58d6cdb01a7b476657a2cfefe7f645d02
Catalin-Andrei BORA <catalin.bora@audionowdigital.com>
2015-12-16 14:16:33 +0200

    work on refactoring

---
+++ Client/BikeHack/BikeHack/Controller/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  BikeHack
+//
+//  Created by Catalin-Andrei BORA on 11/9/15.
+//  Copyright © 2015 The Planeteers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
